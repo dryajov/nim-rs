4d8e3499a25cc8c7e24cea98286d36d626aeeed4
Dmitriy Ryajov <dryajov@gmail.com>
2021-09-06 22:13:35 -0600

    simple arithmetic tests for GF(2^8)

--- tests/testgf.nim
+++ tests/testgf.nim
@@ -2,6 +2,8 @@ import pkg/unittest2
 
 import ../rs/gf
 
+# TODO: Add tests for fields other than GF(2^8) and generator 0x11D
+
 suite "GF Arithmetic":
 
   test "Addition":
@@ -13,17 +15,29 @@ suite "GF Arithmetic":
   test "Multiplication":
     check:
       50.GFUint * 50.GFUint == 109.GFUint
-      # 150.GFUint * 200.GFUint == 118.GFUint
-      # 5.GFUint * 233.GFUint == 233.GFUint
+      150.GFUint * 200.GFUint == 118.GFUint
+      5.GFUint * 233.GFUint == 106.GFUint
 
   test "Substraction":
-    discard
+    check:
+      50.GFUint - 50.GFUint == 0.GFUint
+      150.GFUint - 200.GFUint == 94.GFUint
+      5.GFUint - 233.GFUint == 236.GFUint
 
   test "Divission":
-    discard
+    check:
+      50.GFUint / 50.GFUint == 1.GFUint
+      150.GFUint / 200.GFUint == 22.GFUint
+      5.GFUint / 233.GFUint == 185.GFUint
 
   test "Exponentiation":
-    discard
+    check:
+      50.GFUint ^ 50 == 116.GFUint
+      150.GFUint ^ 200 == 193.GFUint
+      5.GFUint ^ 233 == 255.GFUint
 
   test "Inverse":
-    discard
+    check:
+      GFUint(50).inverse == 111.GFUint
+      GFUint(150).inverse == 15.GFUint
+      GFUint(5).inverse == 167.GFUint
