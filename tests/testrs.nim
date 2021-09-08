a066c239960b279e5c22003ab5121025a2014597
Dmitriy Ryajov <dryajov@gmail.com>
2021-09-08 15:04:26 -0600

    adding initial tests for rs

--- tests/testrs.nim
+++ tests/testrs.nim
@@ -1,12 +1,30 @@
-# This is just an example to get you started. You may wish to put all of your
-# tests into a single file, or separate them into multiple `test1`, `test2`
-# etc. files (better names are recommended, just make sure the name starts with
-# the letter 't').
-#
-# To run these tests, simply execute `nimble test`.
+import sequtils
+import pkg/unittest2
 
-import unittest
+import ../rs/rs
 
-import poly
-test "can add":
-  check add(5, 5) == 10
+suite "Test Reed-Solomon Coding":
+  test "Generator polynomials":
+    check:
+      generator(10) == @[
+        1.GFUint, 216.GFUint, 194.GFUint,
+        159.GFUint, 111.GFUint, 199.GFUint,
+        94.GFUint, 95.GFUint, 113.GFUint,
+        157.GFUint, 193.GFUint]
+
+      generator(8) == @[
+        1.GFUint, 255.GFUint, 11.GFUint,
+        81.GFUint, 54.GFUint, 239.GFUint,
+        173.GFUint, 200.GFUint, 24.GFUint]
+
+  test "Encode":
+    let msg = @[
+      72, 101, 108, 108, 111, 32, 82, 101,
+      101, 100, 45, 83, 111, 108, 111, 109,
+      111, 110, 33].mapIt(it.GFUint)
+
+    check encode(msg, 10) == @[
+      72, 101, 108, 108, 111, 32, 82, 101,
+      101, 100, 45, 83, 111, 108, 111, 109,
+      111, 110, 33, 10, 54, 200, 1, 174,
+      73, 223, 252, 169, 147].mapIt(it.GFUint)
