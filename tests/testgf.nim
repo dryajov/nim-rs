32a18c8948ad678400238f71940041f1b0c3ce2e
Dmitriy Ryajov <dryajov@gmail.com>
2022-01-25 19:14:49 -0600

    skip 2^18 and 2^20 arithmetic tests in i386

--- tests/testgf.nim
+++ tests/testgf.nim
@@ -1,10 +1,11 @@
+import std/os
 import gftype
 
 when Exp == 8:
   import ./testgf8
 elif Exp == 16:
   import ./testgf16
-elif Exp == 18:
+elif Exp == 18 and not defined(i386):
   import ./testgf18
-elif Exp == 20:
+elif Exp == 20 and not defined(i386):
   import ./testgf20
